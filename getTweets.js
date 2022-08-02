cdc72bcff3a792fb366e2b5c15be35843b836480
Cierra-Runis <2864283875@qq.com>
2022-08-02 18:30:09 +0800

    first commit

---
+++ getTweets.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name            推文扒取
+// @namespace       https://github.com/Cierra-Runis/getTweets
+// @version         1.0
+// @description     扒取推文的插件
+// @updateURL       https://raw.githubusercontent.com/Cierra-Runis/FuckQQ/master/FuckQQ.js
+// @downloadURL     https://raw.githubusercontent.com/Cierra-Runis/FuckQQ/master/FuckQQ.js
+// @connect         raw.githubusercontent.com
+// @connect         github.com
+// @connect         cdn.jsdelivr.net
+// @author          https://github.com/Cierra-Runis
+// @match           https://twitter.com/*
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant           none
+// ==/UserScript==
+
+window.onload = (function () {
+    'use strict';
+
+    var list = [];
+    var times = 0;
+    setInterval(addButton, 1000);
+
+    function addButton() {
+        var div = document.createElement('div');
+        div.style.cssText = "width: 25px;height: 25px;color: #ffffff;text-align: center;font-size: small;line-height: 25px;margin: 5px;cursor: pointer;display: flow-root;z-index: 1";
+        div.innerText = "下载";
+        div.onclick = function () {
+            if (div.onclick = confirm("要下载推文吗？")) {
+                setInterval(getTweets, 5000);
+            }
+        }
+        if (document.querySelector('#react-root').lastChild.innerText == '下载') {
+            // console.log('button has existed');
+        } else {
+            document.querySelector('#react-root').appendChild(div);
+        }
+
+    }
+
+    function getTweets() {
+        var url = window.location.href;
+        var before = list.length;
+
+        try {
+            var div1 = document.querySelector("#react-root > div > div ");
+            var div2 = div1.childNodes[2];
+            var main = div2.querySelector('main');
+            var timeline = main.querySelector('div > div > div > div > div');
+            var div3 = timeline.childNodes[1];
+            var div4 = div3.querySelector('div');
+            var div5 = div4.querySelector('div');
+            var section = div5.childNodes[2];
+            var blocks = section.querySelector('div>div').childNodes;
+
+            for (var i = 0; i < blocks.length; i++) {
+                var div6 = blocks[i].querySelector('div>div>div>div>div>div>article>div>div>div');
+                if (div6 == null) {
+                    // console.log('block is null');
+                } else {
+                    try {
+                        var block = div6.childNodes[1];
+                        var href = block.getElementsByTagName("a")[0].href;
+                        if (href != url) {
+                            // console.log(href + "!=" + url);
+                        } else {
+                            var textContent = block.querySelectorAll('div')[51].textContent;
+                            var isExited = false;
+                            for (var j = 0; j < list.length; j++) {
+                                if (list[j].content == textContent) {
+                                    // console.log('text (' + list[j].content + ') has existed!');
+                                    isExited = true;
+                                }
+                            }
+                            if (!isExited) {
+                                console.log('text hasn\'t existed, adding to list.');
+                                list.push({ content: textContent });
+                            }
+                        }
+                    } catch (error) {
+                        // console.log('href isn\'t exist');
+                    }
+                }
+                // console.log(i + ". end");
+            }
+            // console.log('END');
+            var after = list.length;
+            if (after == before) {
+                console.log('No new text was add.');
+                times++;
+            }
+            if (times > 3) {
+                var jsonStr = JSON.stringify(list);
+                // console.log(jsonStr);
+                createAndDownloadFile(url.substring(20) + ".json", jsonStr);
+                while (1);
+            }
+            window.scrollTo(0, document.body.scrollHeight);
+        } catch (error) {
+            console.log('error');
+        }
+
+    }
+
+    function createAndDownloadFile(fileName, content) {
+        var aTag = document.createElement('a');
+        var blob = new Blob([content]);
+        aTag.download = fileName;
+        aTag.href = URL.createObjectURL(blob);
+        aTag.click();
+        URL.revokeObjectURL(blob);
+    }
+
+})();
